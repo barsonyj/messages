e3e7fb053684d82b75ce22d30c9525d947eafc31
Bársony József <barsony@kkando.hu>
2025-01-15 07:46:33 +0100

    First version

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Layout from './Layout.jsx'
+import Messages from './pages/Messages.jsx'
+import Users from './pages/Users.jsx'
+import About from './pages/About.jsx'
+import Login from './pages/Login.jsx'
+import Notfound from './pages/Notfound.jsx'
+
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebaseConfig.js";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export default function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => setUser(currentUser));
+    return () => unsubscribe;
+  },[]);
+
+  async function logout() {
+    await signOut(auth);
+  }
+
+  const router = createBrowserRouter([
+    { path: "/", element: <Layout user={user} logout={logout} auth={auth} />, children:[
+      { path: "/", element: <Messages user={user} db={db} /> },
+      { path: "/users", element: <Users /> },
+      { path: "/about", element: <About /> },
+      { path: "/login", element: <Login auth={auth} /> },
+      { path: "*", element: <Notfound /> }
+    ]}
+  ]);
+
+  return (
+    <div className='app'>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
