f650363b0fc3035ac3f03646d457eea9c38697ce
Bársony József <barsony@kkando.hu>
2025-01-15 08:50:20 +0100

    Version 1.0.0

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
     <div className='about'>
-      About
+      <p><b>Messages v1.0.0</b></p>
+      <p>
+        Üzenetküldő alkalmazás React Vite keretrendszer és Firebase használatával.
+      </p>
+      <ul>
+        <li><Link to="/">Messages</Link>: itt láthatók a küldött/fogadott üzenetek</li>
+        <li><Link to="/users">Users</Link>: felhasználók listája</li>
+        <li><Link to="/about">About</Link>: alkalmazás leírása</li>
+        <li><Link to="/login">Login</Link>: bejelentkezés az alkalmazásba</li>
+      </ul>
     </div>
   )
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 
-export default function Users() {
+export default function Users({db}) {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const snap = await getDocs(query(collection(db, "users"), orderBy("nev")));
+      setUsers(snap.docs.map(doc => ({ ...doc.data(), id:doc.id })));
+    }
+    getUsers();
+  },[]);
+
   return (
     <div className='users'>
-      Users
+      <ul>
+        {users.map(x => <li key={x.id}>{x.nev} ({x.email})</li>)}
+      </ul>
     </div>
   )
 }
